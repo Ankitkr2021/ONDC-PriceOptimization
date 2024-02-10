03a61c3678908afeb625a797fdba230ac8ce5605
Ankit Kumar <108288022+Ankitkr2021@users.noreply.github.com>
2024-02-10 19:17:11 +0530

    Added Product Details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
+
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -36,7 +38,7 @@ const router = createBrowserRouter([
     element: <Checkout></Checkout>,
   },
   { 
-    path: '/product-detail',
+    path: '/product-detail/:id',
     element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
@@ -50,3 +52,60 @@ function App() {
 }
 
 export default App;
+
+// import React from 'react';
+// import { Provider } from 'react-redux'; // Import the Provider component
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from 'react-router-dom';
+// import Home from './pages/Home';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/SignupPage';
+// import Cart from './features/cart/Cart';
+// import CartPage from './pages/CartPage';
+// import Checkout from './pages/Checkout';
+// import ProductDetailPage from './pages/ProductDetailPage';
+// import store from './store'; // Import your Redux store
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home></Home>,
+//   },
+//   {
+//     path: '/login',
+//     element: <LoginPage></LoginPage>,
+//   },
+//   {
+//     path: '/signup',
+//     element: <SignupPage></SignupPage>,
+//   },
+//   { 
+//     path: '/cart',
+//     element: <CartPage></CartPage>,
+//   },
+//   { 
+//     path: '/checkout',
+//     element: <Checkout></Checkout>,
+//   },
+//   { 
+//     path: '/product-detail',
+//     element: <ProductDetailPage></ProductDetailPage>,
+//   },
+// ]);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* Wrap your entire application with the Provider component */}
+//       <Provider store={store}>
+//         <RouterProvider router={router} />
+//       </Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
