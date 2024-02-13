7c7b360eea6b523cbd6aca0e71e2ebb8a2109368
Ankit Kumar <108288022+Ankitkr2021@users.noreply.github.com>
2024-02-14 02:20:18 +0530

    User API, Login/SignUp Page

--- src/app/store.js
+++ src/app/store.js
@@ -1,25 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from '../features/product-list/productSlice';
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
+    auth: authReducer
   },
   
 });
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import productReducer from '../features/product-list/productSlice';
-// import { composeWithDevTools } from 'redux-devtools-extension'; // Import composeWithDevTools from redux-devtools-extension
-
-// const store = configureStore({
-//   reducer: {
-//     product: productReducer,
-//     // Add other reducers if you have them
-//   },
-//   // Enhance the store with the Redux DevTools extension
-//   devTools: process.env.NODE_ENV !== 'production', // Enable only in development
-//   enhancers: [composeWithDevTools()],
-// });
-
-// export default store;
